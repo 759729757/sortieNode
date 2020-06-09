e42747c348d14bf7f25707c04cb84d56f37841b2
759729757 <759729757@qq.com>
2020-06-10 00:04:55 +0800

    'init'

---
+++ mongoose.js
@@ -0,0 +1,13 @@
+﻿var mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/wechat-mall', { useNewUrlParser: true }) //服务器
+mongoose.Promise = global.Promise
+module.exports = mongoose
+
+
+
+
+
+
+
+
+
